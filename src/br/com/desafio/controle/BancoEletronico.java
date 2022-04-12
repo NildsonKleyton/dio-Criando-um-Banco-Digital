3c6670ac7acaa635df21cd07a7e875ed03c18747
Nildson Kleyton <nildsonkleyton@hotmail.com>
2022-04-11 22:56:16 -0300

    Criando-um-Banco-Digital criando menu

--- src/br/com/desafio/controle/BancoEletronico.java
+++ src/br/com/desafio/controle/BancoEletronico.java
@@ -1,11 +1,27 @@
 package br.com.desafio.controle;
 
+import br.com.desafio.entidades.Cliente;
+import br.com.desafio.entidades.Conta;
+import br.com.desafio.entidades.ContaCorente;
+import br.com.desafio.entidades.ContaPoupanca;
+
 import java.util.Scanner;
 
 public class BancoEletronico {
     public static void main(String[] args) {
+        menuBancos();
+        getOpcaoUsuario();
+
+        menuCliente();
+        getOpcaoUsuario();
+
+        Cliente cliente = new Cliente();
+        Conta cc = new ContaCorente(cliente);
+        Conta cp = new ContaPoupanca(cliente);
+
 
     }
+
     public static int getInt(String mensagem) {
         Scanner scanner = new Scanner(System.in);
         System.out.print(mensagem);
@@ -24,18 +40,31 @@ public class BancoEletronico {
         return scanner.nextLine();
     }
 
-    //criando menu
-    public static void MenuCliente() {
+    public static int getOpcaoUsuario() {
 
-        System.out.println("\n**********Menu de Cliente**********\n");
-        System.out.println("1 - Cadastrar Cliete");
-        System.out.println("2 - Listar Cliete por código");
-        System.out.println("3 - Editar Cliete");
-        System.out.println("4 - Excluir Cliete");
-        System.out.println("5 - Voltar ao menu anterior");
+        return getInt("\nInforme a opção escolhida: ");
     }
 
-    public static int getOpcaoUsuario() {
-        return getInt("\nInforme a opção escolhida: ");
+    /**
+     * Menu cliente
+     */
+    public static void menuCliente() {
+
+        System.out.println("\n*========== Menu de Cliente ==========*");
+        System.out.println("\t1 - Cadastrar Cliete");
+        System.out.println("\t2 - Listar Cliete por código");
+        System.out.println("\t3 - Editar Cliete");
+        System.out.println("\t4 - Excluir Cliete");
+        System.out.println("\t5 - Voltar ao menu anterior");
+    }
+
+    /**
+     * Menu Banco
+     */
+    public static void menuBancos() {
+        System.out.println("\n*========== Menu de Banco ==========*");
+        System.out.println("\t1 - NetBank");
+        System.out.println("\t2 - VirtualBank");
+        System.out.println("\t3 - CryptoBank");
     }
 }
